37f47773ce88afaf8570e3c56a4a5a6ff47b666d
falvad01 <falvad01@estudiantes.unileon.es>
2020-02-18 22:14:08 +0100

    barra de temperatura e intento de widget

--- app/src/main/java/es/dc/pws/MainActivity.java
+++ app/src/main/java/es/dc/pws/MainActivity.java
@@ -1,16 +1,23 @@
 package es.dc.pws;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    double temperature = -10;
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,15 +29,58 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
-        Web w = new Web();
-        TextView temp = findViewById(R.id.temperature);
 
-        temp.setText(String.valueOf(w.getTemperature()) + " ºC");
+            Web w = new Web();
+            TextView temp = findViewById(R.id.temperature);
 
-        TextView hum = findViewById(R.id.humidity);
+             temperature = w.getTemperature();
+            //temperature = -10;
+            ProgressBar progresTemp = findViewById(R.id.progresBarTemperature);
+            progresTemp.setProgress((int) temperature);
 
-        hum.setText(String.valueOf(w.getHumidity()) + "%");
 
+            if (temperature <= 2) {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.BLUE, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.BLUE);
+            } else if (temperature > 2 && temperature <= 15) {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.CYAN, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.CYAN);
+            } else if (temperature > 15 && temperature <= 20) {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.GREEN, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.GREEN);
+            } else if (temperature > 20 && temperature <= 25) {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.YELLOW, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.YELLOW);
+            } else if (temperature > 25) {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.RED, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.RED);
+            } else {
+                Drawable progressDrawable = progresTemp.getProgressDrawable().mutate();
+                progressDrawable.setColorFilter(Color.GRAY, android.graphics.PorterDuff.Mode.SRC_IN);
+                progresTemp.setProgressDrawable(progressDrawable);
+                temp.setTextColor(Color.GRAY);
+            }
+
+
+            Log.i("INFO", (String.valueOf(temperature)));
+
+            temp.setText(String.valueOf(temperature) + " ºC");
+
+            TextView hum = findViewById(R.id.humidity);
+
+            hum.setText(String.valueOf(w.getHumidity()) + "%");
+
+            temperature = temperature + 0.2;
 
 
 
